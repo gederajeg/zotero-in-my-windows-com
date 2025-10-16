45328b41524cec237f519c1ee99d81596df7413b
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-10-16 19:18:43 +0800

    Update file list.

--- Zotero-Storage-PDF-check-script.sh
+++ Zotero-Storage-PDF-check-script.sh
@@ -1,1 +1,1 @@
-ls /c/Users/GRajeg/Zotero -R | grep "\.pdf$" > /c/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/Zotero-Storage-PDF-file.txt
+ls /c/Users/GRajeg/Zotero -R | grep "\.pdf$" > /c/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/zotero-in-my-windows-com/Zotero-Storage-PDF-file.txt
