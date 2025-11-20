0a2bd451fec5a09be582dfbd7455028a89f06332
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-11-20 23:11:44 +0800

    Update.

--- Zotero-Storage-PDF-check-script.sh
+++ Zotero-Storage-PDF-check-script.sh
@@ -1,1 +1,11 @@
 ls /c/Users/GRajeg/Zotero -R | grep "\.pdf$" > /c/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/zotero-in-my-windows-com/Zotero-Storage-PDF-file.txt
+
+diff --brief <(sort Zotero-Storage-PDF-file.txt) <(sort Zotero-Storage-PDF-file-MASTER.txt)
+
+# cmp -s "Zotero-Storage-PDF-file.txt" "Zotero-Storage-PDF-file-MASTER.txt"
+
+comm -3 "Zotero-Storage-PDF-file.txt" "Zotero-Storage-PDF-file-MASTER.txt"
+
+comm -3 <(sort Zotero-Storage-PDF-file.txt) <(sort Zotero-Storage-PDF-file-MASTER.txt)
+
+cp /c/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/zotero-in-my-windows-com/Zotero-Storage-PDF-file.txt /c/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/zotero-in-my-windows-com/Zotero-Storage-PDF-file-MASTER.txt
